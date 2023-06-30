6bbdedb8fee954493fd4ced6c4d88cf0ced94e43
Ivy Reed <idcfu788@gmail.com>
2023-06-29 20:25:03 -0400

    more fixing seeds

--- models/Search.js
+++ models/Search.js
@@ -2,34 +2,36 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../db/config');
 
-class Search extends Model { }
+class Search extends Model {}
 
 Search.init(
-    // define columns
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        user_id: {
-            type: DataTypes.UUID,
-            references: {
-                model: 'user',
-                key: 'id',
-                unique: false
-            }
-        },
-
+  // define columns
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Search',
-    }
+    user_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: 'User',
+        key: 'id',
+        unique: false,
+      },
+    },
+    query: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Search',
+  }
 );
 
 module.exports = Search;

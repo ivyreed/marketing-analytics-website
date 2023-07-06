73e882441fe40d678a657e1f5803392004b8ca9b
Houston Culpepper <howacul@gmail.com>
2023-07-05 21:01:08 -0400

    fixed pagecontroller routes and get and post for projects'

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -4,14 +4,22 @@ const { User } = require('../models');
 
 module.exports = {
   getDashboard: async (req, res) => {
+    // console.log(req.session);
     const user = await User.findOne({
       where: {
         id: req.session.user_id,
-        // include model project
       },
+      include: [
+        {
+          model: Project,
+          attributes: ['id', 'name'],
+        },
+      ],
     });
+    const formattedUser = user.get({ plain: true });
+    console.log('rendering the dashboard');
     res.render('dashboard', {
-      user: user,
+      user: formattedUser,
       welcomeMessage: `Welcome to the dashboard ${req.session.currentUser.firstName}!`,
       isAuthenticated: req.session.isAuthenticated,
     });
@@ -19,11 +27,17 @@ module.exports = {
 
   getProjects: async (req, res) => {
     const {
-      body: { user_id, name },
+      body: { user_id },
     } = req;
     try {
       const project = await Project.findAll({
-        where: { user_id, name },
+        where: { user_id },
+        include: [
+          {
+            model: Project,
+            attributes: ['name'],
+          },
+        ],
       });
 
       res.status(200).json(project);
@@ -36,11 +50,14 @@ module.exports = {
     const {
       body: { user_id, name },
     } = req;
+    console.log(name);
+    console.log(req.session.user_id);
     try {
       const project = await Project.create({
-        where: { user_id, name },
+        user_id: `${req.session.user_id}`,
+        name,
       });
-
+      console.log(project);
       res.status(200).json(project);
     } catch (err) {
       res.status(400).json(err);
